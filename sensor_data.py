2f0ba2282f88a10f2f54b6671169c91561a346b7
earthlertkit <earthlertkit@gmail.com>
2025-01-01 15:22:05 -0500

    Added gyroscope update

--- sensor_data.py
+++ sensor_data.py
@@ -1,16 +1,23 @@
 import numpy as np
 from scipy.integrate import solve_ivp
+import dynamics
 
 class Gyroscope:
     def __init__(self, omega_init, measurement_rate):
         self.omega = omega_init
         self.dt = measurement_rate
-
-    def rotational_dynamics(self, torque, params):
-        I = params["MomentOfInertia"]
-        omega_dot = np.linalg.solve(I, torque - np.cross(self.omega, I @ self.omega))
-
-        return omega_dot
         
     def update(self, torque, params):
-        pass
+
+        # Rotational dynamics solver
+        solution = solve_ivp(
+            dynamics.rotational_dynamics,
+            (0.0, self.dt),
+            self.omega,
+            args=(torque, params),
+            t_eval=np.linspace(0.0, self.dt, 10),
+            method="RK45"
+        )
+
+        # Update
+        self.omega = solution.y[:, -1]
