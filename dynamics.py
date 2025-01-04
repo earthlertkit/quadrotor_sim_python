c7219ecc23ac083ef4c82659385372b95559ad6f
earthlertkit <earthlertkit@gmail.com>
2025-01-03 22:34:35 -0500

    Added drone dynamics

--- dynamics.py
+++ dynamics.py
@@ -1,7 +1,33 @@
 import numpy as np
+import quaternion_math as qt
 
 def rotational_dynamics(t, omega, torque, params):
     I = params["MomentOfInertia"]
     omega_dot = np.linalg.solve(I, torque - np.cross(omega, I @ omega))
 
     return omega_dot
+
+def quadrotor_dynamics(t, x, omega, acc, params):
+    q = x[0:4]
+    v = x[7:10]
+
+    # Quaternions
+    q_dot = 0.5 * np.array([[0, -omega[0], -omega[1], -omega[2]],
+                            [omega[0], 0, omega[2], -omega[1]],
+                            [omega[1], -omega[2], 0, omega[0]],
+                            [omega[2], omega[1], -omega[0], 0]]) @ q
+
+    # Positions
+    r_dot = v
+
+    # Velocities
+    v_dot = qt.rot2quat(q).T @ acc + params["gravity"]
+
+    # Accelerometer biases
+    b_a_dot = np.zeros(3)
+
+
+    # Gyroscope biases
+    b_w_dot = np.zeros(3)
+
+    return np.row_stack((q_dot, r_dot, v_dot, b_a_dot, b_w_dot))
