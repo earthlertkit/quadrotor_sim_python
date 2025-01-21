e647022fa6ff2c4d0a4aa961448ef8355ba7af3a
earthlertkit <earthlertkit@gmail.com>
2025-01-20 21:56:50 -0500

    Added update and minor changes

--- motor_model.py
+++ motor_model.py
@@ -1,8 +1,11 @@
 import numpy as np
+from scipy.integrate import solve_ivp
+import dynamics
 
 class Motor:
-    def __init__(self, rpm_init):
-        self.rpm
+    def __init__(self):
+        self.rpm = 0
+        self.rpm_desired = 0
 
     def control(self, thrust, torque, params):
         
@@ -12,8 +15,8 @@ class Motor:
         l = params["moment_arm_length"]
 
         # RPM limits
-        rpm_max = 5000
-        rpm_min = 500
+        rpm_max = 10
+        rpm_min = 0
 
         # Calculating desired RPM to send to motor
         A = np.array([ct, ct, ct, ct],
@@ -23,22 +26,32 @@ class Motor:
         
         b = np.concatenate((thrust, torque))
 
-        rpm_desired = np.sqrt(np.max(np.linalg.solve(A, b), 0))
+        self.rpm_desired = np.sqrt(np.max(np.linalg.solve(A, b), 0))
 
         # Applying RPM limits
-        rpm_desired = np.min(np.max(rpm_desired, rpm_min), rpm_max)
+        self.rpm = np.clip(self.rpm, rpm_min, rpm_max)
+        self.rpm_desired = np.clip(self.rpm_desired, rpm_min, rpm_max)
 
         # Calculate thrust and torque produced by the motor
-        thrust_motor = ct * np.sum(rpm_desired**2)
-        torque_motor = np.array([l*ct*(rpm_desired[2]**2 - rpm_desired[4]**2),
-                                 l*ct*(rpm_desired[3]**2 - rpm_desired[1]**2),
-                                 cq*(-rpm_desired[1]**2 + rpm_desired[2]**2 - rpm_desired[3]**2 + rpm_desired[4]**2)])
+        thrust_motor = ct * np.sum(self.rpm**2)
+        torque_motor = np.array([l*ct*(self.rpm[2]**2 - self.rpm[4]**2),
+                                 l*ct*(self.rpm[3]**2 - self.rpm[1]**2),
+                                 cq*(-self.rpm[1]**2 + self.rpm[2]**2 - self.rpm[3]**2 + self.rpm[4]**2)])
         
         return thrust_motor, torque_motor
 
     
-    def update(self):
-        pass
-    
-    def get_rpm(self):
-        return self.rpm
+    def update(self, params):
+
+        # Rotational dynamics solver
+        solution = solve_ivp(
+            dynamics.rotational_dynamics,
+            (0.0, self.dt),
+            self.rpm,
+            args=(self.rpm_desired, params),
+            t_eval=[self.dt],
+            method="RK45"
+        )
+
+        # Update
+        self.rpm = solution.y[:, -1]
