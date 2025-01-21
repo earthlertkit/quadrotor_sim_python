b4ad77ae5bd99617b0d208f2e71f966af0dcff24
earthlertkit <earthlertkit@gmail.com>
2025-01-20 21:48:43 -0500

    Added quaternion to euler angle conversion

--- quaternion_math.py
+++ quaternion_math.py
@@ -52,3 +52,25 @@ def multiply(q1, q2):
     q3_3 = q1[0] * q2[3] + q1[1] * q2[2] - q1[2] * q2[1] + q1[3] * q2[0]
 
     return np.array([q3_0, q3_1, q3_2, q3_3])
+
+def quat2eul(q):
+    q_w, q_x, q_y, q_z = q
+
+    # Roll (x-axis rotation)
+    sinr_cosp = 2 * (q_w * q_x + q_y * q_z)
+    cosr_cosp = 1 - 2 * (q_x**2 + q_y**2)
+    roll = np.arctan2(sinr_cosp, cosr_cosp)
+
+    # Pitch (y-axis rotation)
+    sinp = 2 * (q_w * q_y - q_z * q_x)
+    if np.abs(sinp) >= 1:
+        pitch = np.sign(sinp) * np.pi / 2  # Clamp to 90 degrees
+    else:
+        pitch = np.arcsin(sinp)
+
+    # Yaw (z-axis rotation)
+    siny_cosp = 2 * (q_w * q_z + q_x * q_y)
+    cosy_cosp = 1 - 2 * (q_y**2 + q_z**2)
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
+
+    return np.array([roll, pitch, yaw])
