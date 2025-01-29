Fixed desired quaternion issue

attitude_controller.py
@@ -10,7 +10,7 @@ class P2:
 
     def control(self, state_current, state_desired, params, acc, omega):
         # Quaternion error
-        q_desired = self.desired_quaternions(state_desired, params, acc)
+        q_desired = self.desired_quaternions(state_current, state_desired, params, acc)
         q_current = state_current[0:4]
         q_error = qt.multiply(q_desired, qt.conjugate(q_current))[1:4]
 
@@ -23,24 +23,21 @@ class P2:
 
         return torque, state_desired_new
 
-    def desired_quaternions(self, state_desired, params, acc):
-        # Desired z-axis (unit thrust vector)
-        T = acc - params["gravity"]
-        z_b_desired = T / np.linalg.norm(T)
+    def desired_quaternions(self, state_current, state_desired, params, acc):
+        # Yaw desired
+        yaw_desired = qt.quat2eul(state_desired[0:4])[2]
 
-        # Yaw rotation x-axis
-        R_yaw = qt.quat2rot(state_desired[0:4])
-        x_b_yaw = R_yaw[:, 0]
+        # Computing DCM
+        z_b = acc - params["gravity"]
+        z_b /= np.linalg.norm(z_b)
+
+        x_c = np.array([np.cos(yaw_desired), np.sin(yaw_desired), 0])
+
+        y_b = np.cross(z_b, x_c) / np.linalg.norm(np.cross(z_b, x_c))
+
+        x_b = np.cross(y_b, z_b)
+
+        R = np.array([x_b, y_b, z_b])
         
-        # Desired y-axis
-        y_b_desired = np.cross(z_b_desired, x_b_yaw)
-        y_b_desired /= np.linalg.norm(y_b_desired)
-
-        # Desired x-axis
-        x_b_desired = np.cross(y_b_desired, z_b_desired)
-
-        # Desired Rotation Matrix
-        R_desired = np.column_stack((x_b_desired, y_b_desired, z_b_desired))
-
-        return qt.rot2quat(R_desired)
+        return qt.rot2quat(R)
 