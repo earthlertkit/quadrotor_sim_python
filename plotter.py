bd55d70cffe5b57a3d2b04a6983432f5c7968150
earthlertkit <earthlertkit@gmail.com>
2025-01-29 16:17:48 -0500

    Added plotters

---
+++ plotter.py
@@ -0,0 +1,89 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import quaternion_math as qt
+
+def position_plot(t, r_actual, r_desired):
+    fig, ax = plt.subplots(3, 1)
+
+    ax[0].plot(t, r_actual[0], label='Actual')
+    ax[0].plot(t, r_desired[0], label='Desired')
+    ax[0].set_title("x")
+    ax[0].legend()
+
+    ax[1].plot(t, r_actual[1], label='Actual')
+    ax[1].plot(t, r_desired[1], label='Desired')
+    ax[1].set_title("y")
+    ax[1].legend()
+
+    ax[2].plot(t, r_actual[2], label='Actual')
+    ax[2].plot(t, r_desired[2], label='Desired')
+    ax[2].set_title("z")
+    ax[2].legend()
+
+
+def orientation_plot(t, q_actual, q_desired):
+    angles_current_plot = np.zeros((3, len(t)))
+    angles_desired_plot = np.zeros((3, len(t)))
+
+    for j in range(len(t)):
+        angles_current_plot[:, j] = qt.quat2eul(q_actual[:, j])
+        angles_desired_plot[:, j] = qt.quat2eul(q_desired[:, j])
+
+    fig, ax = plt.subplots(3, 1)
+
+    ax[0].plot(t, angles_current_plot[0, :], label='Actual')
+    ax[0].plot(t, angles_desired_plot[0, :], label='Desired')
+    ax[0].set_title("roll")
+    ax[0].legend()
+
+    ax[1].plot(t, angles_current_plot[1, :], label='Actual')
+    ax[1].plot(t, angles_desired_plot[1, :], label='Desired')
+    ax[1].set_title("pitch")
+    ax[1].legend()
+
+    ax[2].plot(t, angles_current_plot[2, :], label='Actual')
+    ax[2].plot(t, angles_desired_plot[2, :], label='Desired')
+    ax[2].set_title("yaw")
+    ax[2].legend()
+
+
+def gyroscope_plot(t, omega):
+    fig, ax = plt.subplots(3, 1)
+
+    ax[0].plot(t, omega[0])
+    ax[0].set_title("angular velocity in x")
+
+    ax[1].plot(t, omega[1])
+    ax[1].set_title("angular velocity in y")
+
+    ax[2].plot(t, omega[2])
+    ax[2].set_title("angular velocity in z")
+
+
+def accelerometer_plot(t, acc):
+    fig, ax = plt.subplots(3, 1)
+
+    ax[0].plot(t, acc[0])
+    ax[0].set_title("acceleration in x")
+
+    ax[1].plot(t, acc[1])
+    ax[1].set_title("acceleration in y")
+
+    ax[2].plot(t, acc[2])
+    ax[2].set_title("acceleration in z")
+
+
+def motor_plot(t, rpm):
+    fig, ax = plt.subplots(4, 1)
+
+    ax[0].plot(t, rpm[0])
+    ax[0].set_title("motor 1 rpm")
+
+    ax[1].plot(t, rpm[1])
+    ax[1].set_title("motor 2 rpm")
+
+    ax[2].plot(t, rpm[2])
+    ax[2].set_title("motor 3 rpm")
+
+    ax[3].plot(t, rpm[3])
+    ax[3].set_title("motor 4 rpm")
