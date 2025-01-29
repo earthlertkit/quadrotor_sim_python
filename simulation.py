91d8de4bc60ecb3e1793af36c4e1c5544073e6f4
earthlertkit <earthlertkit@gmail.com>
2025-01-29 16:19:25 -0500

    Updated simulation

--- simulation.py
+++ simulation.py
@@ -7,7 +7,9 @@ import path_planning
 import position_controller
 import attitude_controller
 import dynamics
-
+import motor_model
+import plotter
+import time as timing
 
 def run_simulation():
 
@@ -20,19 +22,23 @@ def run_simulation():
 
     # Quadrotor parameters
     quadrotor_params = {
-        "mass": 1.0,
+        "mass": 1e-1,
         "gravity": np.array([0, 0, -9.81]),
-        "moment_of_inertia": np.eye(3)
+        "moment_of_inertia": np.eye(3) * 1e-4,
+        "thrust_coeff": 1e-5,
+        "moment_scale": 1e-10,
+        "moment_arm_length": 1e-1,
+        "motor_constant": 36.5
     }
-
+    
     # Sensor parameters
 
-    # Controller parameters
-    Kp = np.array([1, 1, 1])
+    # Controller parameters 
+    Kp = np.array([1, 1, 5])
     Ki = np.array([0, 0, 0])
-    Kd = np.array([1, 1, 1])
-    Kq = 0
-    Kw = 0
+    Kd = np.array([1, 1, 4])
+    Kq = 0.0001
+    Kw = 0.0001
 
     # Initializing sensors
     accelerometer = sensor_data.Accelerometer(-quadrotor_params["gravity"])
@@ -41,6 +47,9 @@ def run_simulation():
     # Initializing controllers
     pos_controller = position_controller.PID(Kp, Ki, Kd)
     att_controller = attitude_controller.P2(Kq, Kw)
+
+    # Initializing motors
+    motor = motor_model.Motor()
 
     # Generating path from waypoints [x, y, z, yaw]
     waypoints = np.array([[0, 0, 0, 0],
@@ -56,10 +65,13 @@ def run_simulation():
     time = []
     state_current_plot = np.zeros((16, path_desired.shape[1]))
     state_desired_plot = np.zeros((16, path_desired.shape[1]))
+    acc_IMU_plot = np.zeros((3, path_desired.shape[1]))
+    omega_IMU_plot = np.zeros((3, path_desired.shape[1]))
+    rpm_plot = np.zeros((4, path_desired.shape[1]))
 
     # Control loop
     for i in range(path_desired.shape[1]):
-        print(i)
+        
         # Get sensor data
         acc_IMU = accelerometer.get_sensor_data()
         omega_IMU = gyroscope.get_sensor_data()
@@ -69,7 +81,7 @@ def run_simulation():
                                                      state_desired=path_desired[:, i], 
                                                      dt=dt, 
                                                      params=quadrotor_params)
-
+        
         # Attitude controller
         torque_req, state_desired = att_controller.control(state_current=state_current,
                                             state_desired=path_desired[:, i],
@@ -77,63 +89,45 @@ def run_simulation():
                                             acc=acc_req,
                                             omega=omega_IMU)
         
+        # Motor model
+        thrust_motor, torque_motor = motor.control(thrust_req, torque_req, quadrotor_params)
+
         # Plotting
         time.append(i*dt)
         state_current_plot[:, i] = state_current
         state_desired_plot[:, i] = state_desired
+        acc_IMU_plot[:, i] = acc_IMU
+        omega_IMU_plot[:, i] = omega_IMU
+        rpm_plot[:, i] = motor.get_rpm_data()
         
+        start = timing.time()
         # Simulating dynamics
         solution = solve_ivp(
             dynamics.quadrotor_dynamics,
             (0.0, dt),
             state_current,
             args=(omega_IMU, acc_IMU, quadrotor_params),
-            t_eval=np.linspace(0.0, dt, 10),
-            method="RK45"
+            t_eval=[dt],
+            method="RK45",
         )
+        end = timing.time()
+        print("Dynamics: ", end-start)
         state_current = solution.y[:, -1]
         state_current[0:4] /= np.linalg.norm(state_current[0:4])
 
+        # Update motors
+        motor.update(quadrotor_params, dt)
+
         # Update sensors
-        gyroscope.update(torque_req, quadrotor_params)
-        accelerometer.update(state_current, thrust_req, quadrotor_params)
+        gyroscope.update(torque_motor, quadrotor_params)
+        accelerometer.update(state_current, thrust_motor, quadrotor_params)
 
-    # Outputting orientation plots
-    fig, ax = plt.subplots(3, 1)
-
-    ax[0].plot(time, state_current_plot[1, :], label='Actual')
-    ax[0].plot(time, state_desired_plot[1, :], label='Desired')
-    ax[0].set_title("x")
-    ax[0].legend()
-
-    ax[1].plot(time, state_current_plot[2, :], label='Actual')
-    ax[1].plot(time, state_desired_plot[2, :], label='Desired')
-    ax[1].set_title("y")
-    ax[1].legend()
-
-    ax[2].plot(time, state_current_plot[3, :], label='Actual')
-    ax[2].plot(time, state_desired_plot[3, :], label='Desired')
-    ax[2].set_title("z")
-    ax[2].legend()
-
-    # Outputting position plots
-    fig, ax = plt.subplots(3, 1)
-
-    ax[0].plot(time, state_current_plot[4, :], label='Actual')
-    ax[0].plot(time, state_desired_plot[4, :], label='Desired')
-    ax[0].set_title("x")
-    ax[0].legend()
-
-    ax[1].plot(time, state_current_plot[5, :], label='Actual')
-    ax[1].plot(time, state_desired_plot[5, :], label='Desired')
-    ax[1].set_title("y")
-    ax[1].legend()
-
-    ax[2].plot(time, state_current_plot[6, :], label='Actual')
-    ax[2].plot(time, state_desired_plot[6, :], label='Desired')
-    ax[2].set_title("z")
-    ax[2].legend()
-
+    # Outputting plots
+    plotter.position_plot(time, state_current_plot[4:7], state_desired_plot[4:7])
+    plotter.orientation_plot(time, state_current_plot[0:4], state_desired_plot[0:4])
+    plotter.gyroscope_plot(time, omega_IMU_plot)
+    plotter.accelerometer_plot(time, acc_IMU_plot)
+    plotter.motor_plot(time, rpm_plot)
     plt.show()
 
 if __name__ == "__main__":
